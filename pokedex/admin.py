051472c302bdce3836b103ca0dc331a7fd053440
Vallejo Cristopher <cristopher.vallejo@uisek.edu.ec>
2025-05-19 17:56:14 -0500

    laboratorio-django-CristopherVallejo

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
+from .models import Pokemon
+
 # Register your models here.
+
+ 
+
+@admin.register(Pokemon)
+
+class PokemonAdmin(admin.ModelAdmin):
+
+    pass
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Pokemon
+# Create your views here.   
 def index(request):
-    pokemons = ['charmander', 'pikachu', 'squirtle']
+    pokemons = Pokemon.objects.order_by('name')
+    # pokemons = Pokemon.objects.all()
     template = loader.get_template('index.html')
     return HttpResponse(template.render({'pokemons': pokemons}, request))
 
-def pokemon(request, pokemon):
+def pokemon(request, id: int):
+    pokemon = Pokemon.objects.get(pk=id)
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
